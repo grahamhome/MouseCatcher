4fb556ebc5869ce9d97bd58efe0e28b27acc7f8c
Justin <jmg8766@gmail.com>
2017-01-08 21:48:30 -0500

    Some code simplification, removing the gui from DataReceiver, updating the protocal to be 1-up, 2-down, 3-left, 4-right.

--- src/DataReceiver.java
+++ src/DataReceiver.java
@@ -1,86 +1,17 @@
-import java.awt.EventQueue;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Enumeration;
-import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingWorker;
-
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 
-@SuppressWarnings("serial")
 public class DataReceiver {
-	
-	private JFrame window;
-	private ServerSocket clientConnection;
-	private JTextArea windowOutput;
-	private BufferedReader input;
-	private final String portName = "COM3"; //Rename to the actual COM port represented by your USB serial port
-	private SerialPort serialPort;
-	private OutputStream output;
-	private String beamOn = "254";
-	private String beamOff = "255";
-	private String separator = "0";
-	
-	public DataReceiver() {
-		createWindow();
-		openSocket();
-		openSerialPort();
-	}
-	
-	private void createWindow() {
-		window = new JFrame();
-		window.setTitle("Data Receiver");
-		window.setSize(400, 400);
-		window.setLocationRelativeTo(null);
-		window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		window.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent event) {
-				quit();
-			}
-		});
-		
-		windowOutput = new JTextArea();
-		windowOutput.setEditable(false);
-		JScrollPane scroll = new JScrollPane(windowOutput, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		window.add(scroll);
-		window.setVisible(true);
-	}
-	
-	private void quit() {
-		window.dispose();
-		try {
-			input.close();
-			clientConnection.close();
-			output.close();
-			serialPort.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			System.exit(0);
-		}
-	}
-	
-	private void openSocket() {
-		try {
-			clientConnection = new ServerSocket(8008);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void openSerialPort() {
+
+	private final static String portName = "/dev/ttyS88";
+	private static SerialPort serialPort;
+	private static OutputStream output;
+
+	private static void openSerialPort() {
 		CommPortIdentifier portID = null;
 		Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
 		while (portEnum.hasMoreElements()) {
@@ -99,71 +30,18 @@ public class DataReceiver {
 			output = serialPort.getOutputStream();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
-	}
-	
-	public void listen() {
-		new DataReader().execute();		
+		}
 	}
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> {
-			DataReceiver dr = new DataReceiver();
-			dr.listen();
-		});	
-	}
-	
-	private class DataReader extends SwingWorker<Void, String> {
-
-		@Override
-		protected Void doInBackground() throws Exception {
-			Socket connectionSocket = clientConnection.accept();
-			input = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			while (true) {
-				try {
-					String data = input.readLine();
-					publish(data + "\n");
-				} catch (Exception e) {
-					e.printStackTrace();
-					Thread.sleep(1); // Give the window time to respond to input so it can be closed
-				}
-			}
+	public static void main(String[] args) throws Exception {
+		ServerSocket clientConnection = new ServerSocket(8008);
+		openSerialPort();
+		InputStream input = clientConnection.accept().getInputStream();
+		int data;
+		while((data = input.read()) != -1) {
+			System.out.println("Sending " + data + " to Arduino");
+			output.write((byte)data);
 		}
-		
-		@Override
-		protected void process(List<String> coords) {
-			for (String point : coords) {
-				point = point.trim();
-				if (point.equals(String.valueOf(true))) {
-					send(beamOn);
-					windowOutput.append(point + "\n");
-				} else if (point.equals(String.valueOf(false))) {
-					send(String.valueOf(beamOff));
-					windowOutput.append(point + "\n");
-				} else {
-					String[] values = point.split(",");
-					if ((values.length == 2) && ! (values[0].equals(beamOn) || 
-							values[0].equals(beamOff) || 
-							values[0].equals(separator) || 
-							values[1].equals(beamOn) || 
-							values[1].equals(beamOff) || 
-							values[1].equals(separator))) {
-						send(separator);
-						send(values[0]);
-						send(values[1]);
-						windowOutput.append(point + "\n");
-					}
-				}
-			}
-		}
-		
-		private void send(String data) {
-			try {
-				System.out.println("Sending " + data + " to Arduino");
-				output.write(Byte.parseByte(data));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		clientConnection.close();
 	}
 }
--- src/MouseCatcher.java
+++ src/MouseCatcher.java
@@ -1,65 +1,38 @@
-import java.awt.EventQueue;
-import java.awt.MouseInfo;
-import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
 public class MouseCatcher {
-	
-	private JFrame window;
-	private boolean active = false;
-	private Socket serverConnection;
-	private DataOutputStream out;
 
-	public MouseCatcher() {
-		createWindow();
-		openSocket();
-		toggleTracking(false);
-		window.addMouseListener(new ClickTracker());
-		window.addMouseMotionListener(new MotionTracker());
-	}
+	private static JFrame window;
+	private static Socket serverConnection;
+	private static DataOutputStream output;
 
-	private void createWindow() {
+	private static void createWindow() {
 		window = new JFrame();
 		window.setSize(800, 800);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		window.addWindowListener(new WindowAdapter() {
-			@Override
 			public void windowClosing(WindowEvent event) {
-				quit();
+				try { serverConnection.close(); } catch (IOException e) {}
 			}
 		});
 		window.setVisible(true);
 	}
 
-	private void quit() {
-		window.dispose();
-		try {
-			serverConnection.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			System.exit(0);
-		}
-	}
-
-	private void openSocket() {
-		window.setTitle("Remote Laser Controller - Connecting To Server...");
+	private static void openSocket() {
+		window.setTitle("Remote Laser Controller");
 		boolean connected = false;
 		do {
 			try {
 				serverConnection = new Socket("localhost", 8008);
-				out = new DataOutputStream(serverConnection.getOutputStream());
+				output = new DataOutputStream(serverConnection.getOutputStream());
 				connected = true;
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -67,78 +40,29 @@ public class MouseCatcher {
 		} while (!connected);
 	}
 
-	private void toggleTracking(Boolean... value) {
-		if (value.length > 0) {
-			if (active != value[0]) {
-				active = value[0];
-				send(String.valueOf(active));
+	public static void main(String[] args) throws Exception {
+		createWindow();
+		openSocket();
+		window.addMouseMotionListener(new MouseAdapter() {
+			private static final byte UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
+			private int prevX, prevY;
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				send(e.getY() < prevY ? UP : DOWN);
+				prevY = e.getY();
+				send(e.getX() < prevX ? LEFT : RIGHT);
+				prevX = e.getX();
 			}
-		} else {
-			active = !active;
-			send(String.valueOf(active));
-		}
-		window.setTitle("Remote Laser Controller - " + (active ? "Active" : "Inactive"));
-	}
 
-	private void reportPosition() {
-		Point mouse = MouseInfo.getPointerInfo().getLocation();
-		Point windowLoc = window.getLocationOnScreen();
-		double x = mouse.getX()-windowLoc.getX();
-		double y = mouse.getY()-windowLoc.getY();
-		if (active) {
-			String coords = (int)((x/window.getWidth())*255) + ", " + (int)((y/window.getHeight())*255);
-			send(coords);
-		}
-	}
-
-	private void send(String data) {
-		try {
-			System.out.println("Sending " + data + " to server");
-			out.writeBytes(data + "\n");
-			out.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> new MouseCatcher());
-	}
-
-	private class ClickTracker implements MouseListener {
-
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-			toggleTracking();
-			if (active) {
-				reportPosition();
+			private void send(byte data) {
+				System.out.println("Sending " + data + " to server");
+				try {
+					output.writeByte(data);
+					output.flush();
+				} catch (IOException e) {
+				}
 			}
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent arg0) {}
-
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-			toggleTracking(false);
-		}
-
-		@Override
-		public void mousePressed(MouseEvent arg0) {}
-
-		@Override
-		public void mouseReleased(MouseEvent arg0) {}
-	}
-
-	private class MotionTracker implements MouseMotionListener {
-
-		@Override
-		public void mouseDragged(MouseEvent e) {}
-
-		@Override
-		public void mouseMoved(MouseEvent e) {
-			reportPosition();
-		}
-
+		});
 	}
 }
